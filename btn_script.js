4489bb8652206ffae6a19601e2892ed8b5030f0e
GabCarps <dgod2504@gmail.com>
2024-09-28 10:53:11 +0200

    Add files via upload

---
+++ btn_script.js
@@ -0,0 +1,7 @@
+
+const showImageBtn = document.getElementById('show_image');
+const image = document.getElementById('image');
+
+showImageBtn.addEventListener('click', function() {
+  image.style.display = 'block';
+});
